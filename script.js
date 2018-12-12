e77784422cbfb57a3c3d0826b1d820bd641aa851
lironbenjamin <lironbenjamin@gmail.com>
2018-12-11 22:08:27 -0500

    searcing of flights and itineraries

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ var root_url = "http://comp426.cs.unc.edu:3001/";
 
 $(document).ready(() => {
     let main = $('.main');
+    let flightItinerary = [];
+    currentFlight = '';
 
     //////Functions to rebuild inteface components /////
-    
     let build_login = function(){
         main.empty();
         $('.navbar').remove();
@@ -62,7 +63,7 @@ $(document).ready(() => {
 
 
 
-///////////Navigation button clicks /////////////////////
+/////////// Navigation button clicks /////////////////////
 ////////////// Button click to build trip planner////////
     $(document).on('click', '.pNav', function () {
         build_trip_planner();
@@ -120,6 +121,8 @@ $(document).ready(() => {
         let dest = '';
         let flights;
         let instances;
+        let route_id = '';
+        let a_id = '';
         $.ajax(root_url + 'airports?filter[code]=' + currentLeg.find('.origin').val(), {
             type: 'GET',
             async: false,
@@ -153,13 +156,15 @@ $(document).ready(() => {
             dataType: 'json',
             success: (response) => {
                 flights = response;
+                route_id = flights[0].id;
+                a_id = flights[0].airline_id;
             },
             error: () => {
             console.log('Failed to find matching Airport');
             }
         });
         
-        let route_id = flights[0].id;
+        
 
         $.ajax(root_url + 'instances?filter[flight_id]=' + route_id, {
             type: 'GET',
@@ -168,20 +173,41 @@ $(document).ready(() => {
             dataType: 'json',
             success: (response) => {
                 instances = response;
+                currentLeg.find('.flights-pane').remove()
+                currentLeg.append("<table class='flights-pane'><tr><th>Airline</th><th>Date</th><th>Flight ID</th></tr></table>");
             },
             error: () => {
             console.log('Failed to find matching Airport');
             }
         });
 
+        
+
         console.log(instances);
         for(i = 0; i < instances.length; i++){
             currentInstance = instances[i];
-
+            tableEntry = "<tr class='flight' fID=" + currentInstance.id + ">" + "<td>" + a_id + "</td><td>" + currentInstance.date + "</td><td>" + currentInstance.id + "</td></tr>";
+            currentLeg.find('.flights-pane').append(tableEntry);
         }
     });
 
+    //////// selecting a flight ///////////////////////////////////
+    $(document).on('click', 'tr', function () {
+        currentFlight = $(this).attr('fID');
+        console.log(currentFlight);
+    });
 
+    /////////// add to itinerary /////////////////////
+    $(document).on('click', '.add', function () {
+        currentLeg = $(this).parents('.leg-section');
+        currentDiv = $(this).parent();
+        let origin = currentLeg.find('.origin').val()
+        let dest = currentLeg.find('.dest').val();
+        addToItinerary(currentFlight, origin, dest);
+        getItinerary();
+    });
+
+    ////////////// get itinerary /////////////////////
 
 
  /////////////////////////////////////////////////////
@@ -189,10 +215,52 @@ $(document).ready(() => {
     //Create Home Page Elements
     let flightsPane = "<div class='flights-pane'></div>"
 
-    let tile = "<div class='leg-section'><div class='insideBox'><p>Origin: <input type='text' class='origin'></p><p>Destination: <input type='text' class='dest'></p><p>Date:</p><button type='button' class='search'>Search Flights</button></div></div>"
+    let tile = "<div class='leg-section'><div class='insideBox'><p>Origin: <input list='airports' class='origin'><datalist id='airports'><option value='BOS'><option value='RDU'><option value='CHI'></datalist></p><p>Destination: <input list='airports' class='dest'><datalist id='airports'><option value='BOS'><option value='RDU'><option value='CHI'></datalist></p><button type='button' class='search'>Search Flights</button><button class='add'>Add Flight to Itinerary</button></div></div>"
 
     let tile2 = "<div class ='leg-section'><div class='insideBox'><div id='floating-panel'><input id='address' type='textbox' value='Sydney, NSW'><input id='submit' type='button' value='Geocode'></div><div id='map'><script async defer src='https://maps.googleapis.com/maps/api/js?key=AIzaSyCwPFArHQ86xloIhaWtkUHNIOQZ2HCcl5s&callback=initMap'></script></div></div>"
 });
+
+let getItinerary = function(){
+    console.log('function ran');
+        $.ajax(root_url + 'itineraries', {
+            type: 'GET',
+            async: false,
+            xhrFields: {withCredentials: true},
+            dataType: 'json',
+            success: (response) => {
+                console.log(response);
+            },
+            error: () => {
+            console.log('Failed to find get Itineraries');
+            }
+        });
+}
+
+let addToItinerary = function(id, origin, dest){
+    console.log('run');
+    $.ajax(root_url + 'itineraries', {
+        type: 'POST',
+        xhrFields: {withCredentials: true},
+        dataType: 'json',
+        data: {
+            "itinerary": {
+                "confirmation_code": stringGen(6),
+                "email":"kmp@cs.unc.edu",
+                "info":{
+                    "flightId": id,
+                    "origin": origin,
+                    "dest": dest
+                }
+              }
+        },
+        success: (response) => {
+            alert("Itinerary Added!")
+        },
+        error: () => {
+        alert('Itinerary Not Added');
+        }
+    });
+}
 
 let getAirportID = function(code){
     $.ajax(root_url + 'airports?filter[code]=' + code, {
@@ -254,3 +322,13 @@ function initMap() {
     });
   }
 
+  function stringGen(len) {
+    var text = "";
+    
+    var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    
+    for (var i = 0; i < len; i++)
+      text += charset.charAt(Math.floor(Math.random() * charset.length));
+    
+    return text;
+  }
