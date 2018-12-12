25b8376bb1a9babd02ed166aa9a39353dde260af
unknown <anerishah534@gmail.com>
2018-12-11 22:34:31 -0500

    added some css

---
+++ AScript.js
@@ -0,0 +1,14 @@
+var root_url = "http://comp426.cs.unc.edu:3001/";
+
+$(document).ready(() => {
+
+    
+let aiportAuto = function(){
+    main.empty();
+    let airportTile = "<div id='page-wrapper'><h1>Airline Options/h1><label for='default' list='Airports' placeholder='e.g. RDU'><datalist id='airports'><option value='BOS'><option value='RDU'><option value='CHI'></datalist></div>"
+    main.append(airportTile);
+}
+
+   
+
+});
